1546204deccba207616bc68bd599e8315a949fa6
Jake Good <jake@simpli.fi>
2012-11-02 11:18:01 -0500

    Slightly more informational

--- lib/appnexusapi/faraday/debug_logger.rb
+++ lib/appnexusapi/faraday/debug_logger.rb
@@ -18,8 +18,8 @@ module AppnexusApi
 
         def call(env)
           info "#{env[:method]} #{env[:url].to_s}"
-          debug('request') { dump_headers env[:request_headers] }
-          debug('request') { env[:body].to_s }
+          debug('request headers') { dump_headers env[:request_headers] }
+          debug('request body') { env[:body].to_s }
           @app.call(env).on_complete do |environment|
             on_complete(environment)
           end
@@ -27,7 +27,7 @@ module AppnexusApi
 
         def on_complete(env)
           info('Status') { env[:status].to_s }
-          debug('response') { dump_headers env[:response_headers] }
+          debug('response headers') { dump_headers env[:response_headers] }
           debug('response body') { env[:body].to_s }
         end
 
