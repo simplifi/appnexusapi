68d737042da6866219851d8bd4837e067ceee1b9
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2015-09-22 03:05:57 -0400

    Add siphon name filtering to log level data service

--- lib/appnexusapi/log_level_data_service.rb
+++ lib/appnexusapi/log_level_data_service.rb
@@ -1,15 +1,17 @@
 class AppnexusApi::LogLevelDataService < AppnexusApi::Service
-  def initialize(connection)
+  def initialize(connection, options = {})
     @read_only = true
-    super
+    @siphon_name = options[:siphon_name]
+
+    super(connection)
   end
 
   def since(time = nil)
-    if time
-      get(updated_since: time.strftime('%Y_%m_%d_%H'))
-    else
-      get
-    end
+    params = {}
+    params[:siphon_name] = @siphon_name if @siphon_name
+    params[:updated_since] = time.strftime('%Y_%m_%d_%H') if time
+
+    get(params)
   end
 
   def uri_name
