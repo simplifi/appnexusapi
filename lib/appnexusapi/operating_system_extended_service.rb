c1b7a68c0488e968bddc421bd3ebcbd5a338b6d4
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2016-06-29 01:51:01 -0400

    Fix plural name for extended OS

--- lib/appnexusapi/operating_system_extended_service.rb
+++ lib/appnexusapi/operating_system_extended_service.rb
@@ -3,4 +3,8 @@ class AppnexusApi::OperatingSystemExtendedService < AppnexusApi::Service
     @read_only = true
     super(connection)
   end
+
+  def plural_name
+    'operating-systems-extended'
+  end
 end
