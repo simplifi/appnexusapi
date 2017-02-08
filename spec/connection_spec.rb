92a9295b8c106a578895065e03bcef53de102a89
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2017-02-08 05:23:08 -0800

    style

--- spec/connection_spec.rb
+++ spec/connection_spec.rb
@@ -4,9 +4,7 @@ describe AppnexusApi::Connection do
   subject { AppnexusApi::Connection.new({}) }
 
   it 'allows no logger to be specified' do
-    expect do
-      AppnexusApi::CreativeService.new(connection_with_null_logger)
-    end.to_not raise_error
+    expect { AppnexusApi::CreativeService.new(connection_with_null_logger) }.to_not raise_error
   end
 
   it 'returns data from expiration' do
@@ -15,9 +13,9 @@ describe AppnexusApi::Connection do
     allow(subject).to receive(:login)
     allow(subject).to receive(:run_request_only) do |arg|
       counter += 1
-      raise AppnexusApi::Unauthorized.new if @counter == 1
-      Faraday::Response.new(body: {not_an_error: 1})
+      raise AppnexusApi::Unauthorized.new if counter == 1
+      Faraday::Response.new(body: { not_an_error: 1 })
     end
-    expect(subject.run_request(:get, "http://localhost", nil, {})).not_to eq({})
+    expect(subject.run_request(:get, 'http://localhost', nil, {})).not_to eq({})
   end
 end
